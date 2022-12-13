938f0e4920ab4776d9831561a7fdc03a3a231ae3
915dbfl <dbfl00915@naver.com>
2022-12-13 23:28:07 +0900

    [feat] 카카오 책 검색 api 사용해 책 검색 기능 추가

--- app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
@@ -1,23 +1,85 @@
 package com.example.booksearchapp.ui.view
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.booksearchapp.databinding.FragmentSearchBinding
+import com.example.booksearchapp.ui.adapter.BookSearchAdapter
+import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
+import com.example.booksearchapp.util.Constants.SEARCH_BOOKS_TIME_DELAY
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding: FragmentSearchBinding get() = _binding!!
 
+    private lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var bookSearchAdapter: BookSearchAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bookSearchViewModel = (activity as MainActivity).bookSearchViewModel
+
+        setupRecyclerView()
+        searchBooks()
+
+        // saerchResult를 관찰하여 변경될 때마다 화면 업데이트
+        bookSearchViewModel.searchResult.observe(viewLifecycleOwner) { response ->
+            val books = response.documents
+            bookSearchAdapter.submitList(books)
+        }
+    }
+
+    //recyclerview 설정
+    private fun setupRecyclerView() {
+        bookSearchAdapter = BookSearchAdapter()
+        binding.rvSearchResult.apply {
+            setHasFixedSize(true)
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+            adapter = bookSearchAdapter
+        }
+    }
+
+    //editText 동작 정의
+    private fun searchBooks() {
+        var startTime = System.currentTimeMillis()
+        var endTime: Long
+
+        binding.etSearch.addTextChangedListener { text: Editable? ->
+            endTime = System.currentTimeMillis()
+            // 처음 입력과 두 번쨰 입력 사이의 시간을 고려
+            if (endTime - startTime >= SEARCH_BOOKS_TIME_DELAY) { // 사람의 입력 시간을 고려해서 검색 실행까지 딜레이를 준다.
+                text?.let {
+                    val query = it.toString().trim()
+                    if (query.isNotEmpty()) {
+                        //editText에 text가 입력되면 값을 viewModel에 전달해 searchBooks를 실행시킨다.
+                        bookSearchViewModel.searchBooks(query)
+                    }
+                }
+            }
+            startTime = endTime
+        }
     }
 
     override fun onDestroy() {
